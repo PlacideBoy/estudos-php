a0a6c498d7763ed8d1194cb43c961d6a143fa97e
Jorge Lucas <83237763+PlacideBoy@users.noreply.github.com>
2022-04-26 20:46:15 -0300

    estrutura repetição While

---
+++ aula11/01-contador.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador While</title>
+</head>
+<body>
+    <?php
+        $c = 1;
+        while ($c <= 10) {
+            echo "$c <br>";
+            $c++;
+        }
+        
+
+        $d = 10;
+        while ($d >= 0) {
+            echo "$d <br>";
+            $d -= 2;
+        }
+    ?>
+</body>
+</html>
